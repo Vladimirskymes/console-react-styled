dd216d9f49fd50454413228f86b50f1021444110
shvets_v <VovaVAV@i.ua>
2021-04-25 22:43:38 +0300

    start, create title component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import styled from "styled-components"
+const AppWrapper = styled.div`
+width: 100%;
+min-height: 100vh;
+background-color: black;
+margin: 0;
+padding: 2rem;
+color: white;
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      asfafa
+    </AppWrapper>
+    
   );
 }
 
